a17a9975b7c54a78dfa5ddea4a150a7d4b3d7c3d
wendy wu <wendy__wu@outlook.com>
2017-07-06 23:36:57 +0800

    7.6

--- 0001/gen_keys.py
+++ 0001/gen_keys.py
@@ -3,9 +3,10 @@ import string, random
 def gen_keys(nums=20):
     keys = []
     for i in range(nums):
-        keys.append("".join([random.choice(string.letters+string.digits) for j in range(20)]))
-    print keys
+        keys.append("-".join("".join([random.choice(string.ascii_letters+string.digits) for j in range(5)]) for k in range(4)))
+    print(keys)
     return keys
-        
+    
 if __name__ =="__main__":
     gen_keys(200)
+    
